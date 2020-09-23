f41789ba7cbd20c48f2aab4cc389ec8b749ba7bb
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-09-23 09:58:43 +0300

    add bugs

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const userList = document.querySelectorAll(".name-list li");
 const listInput = document.querySelector(".list-input");
-const addListBtn = document.querySelector('.addListBtn');
+const addListBtn = document.querySelector(".addListBtn");
 
 addListBtn.addEventListener('click', function () {
-    // Create an li out of thin air
-    const newLi = document.createElement('LI');
+    const newLi = document.createElement("LI");
     const liContent = document.createTextNode(listInput.value);
-
-    // Add the input value inside that new li
     newLi.appendChild(liContent);
-    // Attaching the li to the user 
     userList.appendChild(newLi);
-})
+
+});
+
