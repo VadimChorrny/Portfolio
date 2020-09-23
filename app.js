dc03c85e0d9b0203ff3e96e07dda722ae199419f
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-09-23 09:14:50 +0300

    create cool futures

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const text = document.querySelector('.title');
-const changeColor = document.querySelector('.changeColor');
+const userList = document.querySelectorAll(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector('.addListBtn');
 
-changeColor.addEventListener('click', function () {
-    text.classList.toggle('change')
+addListBtn.addEventListener('click', function () {
+    // Create an li out of thin air
+    const newLi = document.createElement('LI');
+    const liContent = document.createTextNode(listInput.value);
+
+    // Add the input value inside that new li
+    newLi.appendChild(liContent);
+    // Attaching the li to the user 
+    userList.appendChild(newLi);
 })
-
